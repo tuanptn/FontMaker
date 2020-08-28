9f8239fff73dcd1e8d35ba946e2fdca9793080b3
daonguyen207 <54179901+daonguyen207@users.noreply.github.com>
2020-08-28 19:41:31 +0700

    Add files via upload

---
+++ FontMaker.cpp
@@ -0,0 +1,65 @@
+/*
+  FontMaker.cpp - Thư viện Font tương thích utf-8
+  Created by Dao Nguyen - IOT47.com
+  Liên hệ: daonguyen20798@gmail.com
+  Người dùng toàn quyền sử dụng cho mục đích cá nhân
+*/
+#include "FontMaker.h"
+
+MakeFont::MakeFont(set_px_typedef set_px)
+{
+	FontMaker_setpx = set_px;
+}
+void MakeFont::set_font(const MyFont_typedef myfont)
+{
+   _f_name =  myfont.f_name;
+   _f_map =  myfont.f_map;
+}
+unsigned char MakeFont::read(int16_t x, int16_t y,  uint16_t txt)
+{
+    unsigned char temp = x % 8;
+    unsigned char x1 = 0x80 >> temp;
+    unsigned int str = pgm_read_word(_f_map + txt);
+    unsigned char line = pgm_read_byte( str  + _f_name - 2);
+    return pgm_read_byte( str + _f_name + y*line +x/8  )& x1;
+}
+unsigned char MakeFont::putChar(int16_t x,int16_t y,uint16_t txt,uint16_t color,uint16_t backcolor)
+{
+    unsigned int str = pgm_read_word(_f_map + txt);
+    int size_w = pgm_read_byte( str  + _f_name - 4); //lấy chiều rộng Font
+    int size_h = pgm_read_byte( str  + _f_name - 3); //lấy chiều cao Font 
+    int mid_line = pgm_read_byte(_f_name); //lấy mid line - đường cơ bản
+    int start_y=0;
+    int offset = pgm_read_byte( str  + _f_name - 2);
+    if(size_h < mid_line)start_y=mid_line-size_h;
+    start_y += pgm_read_byte( str  + _f_name - 1);
+    for(int i=0;i<size_w;i++)
+        for(int h=0;h<size_h;h++)
+            if(read(i,h,txt) != 0)
+                FontMaker_setpx(i+x,h+y+start_y,color);
+            else FontMaker_setpx(i+x,h+y+start_y,backcolor);
+    return size_w;
+}
+void MakeFont::print(int16_t x,int16_t y,unsigned char *s,uint16_t color,uint16_t backcolor)
+{
+   unsigned char offset=0;
+   uint16_t utf8_addr;
+   while(*s)
+   {
+      utf8_addr=UTF8_GetAddr(s,&offset);
+      x +=putChar(x,y,utf8_addr,color,backcolor)+1;
+      s+=offset;
+   }
+}
+void MakeFont::print(int16_t x,int16_t y,char *s,uint16_t color,uint16_t backcolor)
+{
+   unsigned char offset=0;
+   uint16_t utf8_addr;
+   while(*s)
+   {
+      utf8_addr=UTF8_GetAddr((unsigned char *)s,&offset);
+      x +=putChar(x,y,utf8_addr,color,backcolor)+1;
+      s+=offset;
+   }
+}
+//----------------------------------------------END FILE--------------------------------------------------//
---
+++ MyFontMaker.h
@@ -0,0 +1,27 @@
+/*
+  MyFontMaker.h - Thư viện Font tương thích utf-8 (v2)
+  Created by Dao Nguyen - IOT47.com
+  Liên hệ: daonguyen20798@gmail.com
+  Không sửa file này
+*/
+#ifndef __MY_FontMaker_h___
+#define __MY_FontMaker_h___
+
+#include "Arduino.h"
+
+typedef struct 
+{
+  const uint8_t  *f_name;
+  const uint16_t *f_map;
+}MyFont_typedef;
+
+//Khai báo extern các loại font mà bạn sử dụng
+
+extern const MyFont_typedef MakeFont_Font1;
+//auto add here
+
+
+
+
+#endif //__MY_FontMaker_h___
+//---------------------------------------------------------------------Kết thúc file ----------------------------------------------//
