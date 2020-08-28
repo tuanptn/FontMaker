0b3294ca6f62d90472e274033af53bd7699e4790
daonguyen207 <54179901+daonguyen207@users.noreply.github.com>
2020-08-28 23:03:42 +0700

    Update FontMaker.cpp

--- FontMaker.cpp
+++ FontMaker.cpp
@@ -74,4 +74,54 @@ void MakeFont::print(int16_t x,int16_t y,String str,uint16_t color,uint16_t back
       s+=offset;
    }
 }
+unsigned char MakeFont::putChar_noBackColor(int16_t x,int16_t y,uint16_t txt,uint16_t color)
+{
+    unsigned int str = pgm_read_word(_f_map + txt);
+    int size_w = pgm_read_byte( str  + _f_name - 4); //lấy chiều rộng Font
+    int size_h = pgm_read_byte( str  + _f_name - 3); //lấy chiều cao Font 
+    int mid_line = pgm_read_byte(_f_name); //lấy mid line - đường cơ bản
+    int start_y=0;
+    int offset = pgm_read_byte( str  + _f_name - 2);
+    if(size_h < mid_line)start_y=mid_line-size_h;
+    start_y += pgm_read_byte( str  + _f_name - 1);
+    for(int i=0;i<size_w;i++)
+        for(int h=0;h<size_h;h++)
+            if(read(i,h,txt) != 0)
+                FontMaker_setpx(i+x,h+y+start_y,color);
+    return size_w;
+}
+void MakeFont::print_noBackColor(int16_t x,int16_t y,unsigned char *s,uint16_t color)
+{
+   unsigned char offset=0;
+   uint16_t utf8_addr;
+   while(*s)
+   {
+      utf8_addr=UTF8_GetAddr(s,&offset);
+      x +=putChar_noBackColor(x,y,utf8_addr,color)+1;
+      s+=offset;
+   }
+}
+void MakeFont::print_noBackColor(int16_t x,int16_t y,char *s,uint16_t color)
+{
+   unsigned char offset=0;
+   uint16_t utf8_addr;
+   while(*s)
+   {
+      utf8_addr=UTF8_GetAddr((unsigned char *)s,&offset);
+      x +=putChar_noBackColor(x,y,utf8_addr,color)+1;
+      s+=offset;
+   }
+}
+void MakeFont::print_noBackColor(int16_t x,int16_t y,String str,uint16_t color)
+{
+   unsigned char offset=0;
+   uint16_t utf8_addr;
+   unsigned char *s = &str[0];
+   while(*s)
+   {
+      utf8_addr=UTF8_GetAddr((unsigned char *)s,&offset);
+      x +=putChar_noBackColor(x,y,utf8_addr,color)+1;
+      s+=offset;
+   }
+}
 //----------------------------------------------END FILE--------------------------------------------------//
