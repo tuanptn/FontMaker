8708b4525d5101ff7308fb651d8b2a6351934796
daonguyen207 <54179901+daonguyen207@users.noreply.github.com>
2020-08-28 22:57:59 +0700

    Update FontMaker.h

--- FontMaker.h
+++ FontMaker.h
@@ -18,6 +18,7 @@ class MakeFont
    	unsigned char putChar(int16_t x,int16_t y,uint16_t txt,uint16_t color,uint16_t backcolor);
    	void print(int16_t x,int16_t y,unsigned char *s,uint16_t color,uint16_t backcolor);
    	void print(int16_t x,int16_t y,char *s,uint16_t color,uint16_t backcolor);
+    void print(int16_t x,int16_t y,String str,uint16_t color,uint16_t backcolor);
     void set_font(const MyFont_typedef myfont);
    	MakeFont(set_px_typedef set_px);
    	private:
